45053e402aa9de81aaf8b396cabb42ac0ad8088e
Demitry0-0 <dimka0-0ivanov@yandex.ru>
2023-11-24 04:11:06 +0300

    feat: validate project

--- src/main/kotlin/ru/uniyar/Containers.kt
+++ src/main/kotlin/ru/uniyar/Containers.kt
@@ -1,7 +1,9 @@
 package ru.uniyar
 
 import org.http4k.template.PebbleTemplates
+import ru.uniyar.domain.operations.ProjectService
 import ru.uniyar.domain.operations.UserService
+import ru.uniyar.web.validation.ProjectValidation
 import ru.uniyar.web.validation.UserValidation
 
 class Containers {
@@ -10,8 +12,10 @@ class Containers {
 
         //validate
         val userValidation = UserValidation()
+        val projectValidation = ProjectValidation()
 
         //service
         val userService = UserService()
+        val projectService = ProjectService()
     }
 }
---
+++ src/main/kotlin/ru/uniyar/domain/operations/ProjectService.kt
@@ -0,0 +1,9 @@
+package ru.uniyar.domain.operations
+
+import ru.uniyar.dto.Project
+
+class ProjectService {
+    fun createProject(project: Project) {
+        //save
+    }
+}
