6566e130a0dafc8d61aad468d502898ff8dc23ac
Demitry0-0 <dimka0-0ivanov@yandex.ru>
2023-11-24 16:17:57 +0300

    refactor

--- src/main/kotlin/ru/uniyar/WebApplication.kt
+++ src/main/kotlin/ru/uniyar/WebApplication.kt
@@ -12,73 +12,24 @@ import org.http4k.server.Netty
 import org.http4k.server.asServer
 import org.http4k.template.PebbleTemplates
 import org.http4k.template.TemplateRenderer
-import org.ktorm.dsl.insert
-import ru.uniyar.domain.models.ProjectModel
-import ru.uniyar.domain.models.UserModel
-import ru.uniyar.domain.storage.ProjectManager
-import ru.uniyar.domain.storage.tables.ProjectTable
-import ru.uniyar.domain.storage.tables.SponsorTable
-import ru.uniyar.domain.storage.tables.UserTable
 import ru.uniyar.web.handlers.GetProjectRegistration
-import ru.uniyar.web.handlers.HomeHandler
-import ru.uniyar.web.handlers.ProjectByIdHandler
-import ru.uniyar.web.handlers.ProjectsHandler
 import ru.uniyar.web.handlers.GetUserRegistration
+import ru.uniyar.web.handlers.HomeHandler
 import ru.uniyar.web.handlers.PostProjectRegistration
 import ru.uniyar.web.handlers.PostUserRegistration
-import java.time.LocalDate
-import java.time.LocalDateTime
+import ru.uniyar.web.handlers.ProjectByIdHandler
+import ru.uniyar.web.handlers.ProjectsHandler
 
-fun generateProjects(userModels: List<UserModel>): List<ProjectModel> {
-    val listProjectModels = mutableListOf<ProjectModel>()
-    userModels.first {
-        listProjectModels.add(
-            ProjectModel(
-                0,
-                it,
-                "На анекдоты",
-                "Хочу завести большую базу анекдотов",
-                1000000,
-//                List((1..userModels.size).random()) {
-//                    Pair(userModels.random(), (1000L..1000000L).random())
-//                },
-                LocalDate.of(2010, 10, 1),
-                LocalDate.of(2020, 5, 1),
-            ),
-        )
-    }
-    userModels.last {
-        listProjectModels.add(
-            ProjectModel(
-                1,
-                it,
-                "На кухню",
-                "Хочу улучшить условия труда",
-                9999999,
-//                List((1..userModels.size).random()) {
-//                    Pair(userModels.random(), (1000L..1000000L).random())
-//                },
-                LocalDate.of(2015, 10, 1),
-                LocalDate.of(2017, 5, 1),
-            ),
-        )
-    }
-    return listProjectModels
-}
-
-fun getRoutes(
-    renderer: TemplateRenderer
-): RoutingHttpHandler =
-    routes(
-        "/" bind Method.GET to HomeHandler(renderer),
-        "/projects" bind Method.GET to ProjectsHandler(renderer),
-        "/projects/{id}" bind Method.GET to ProjectByIdHandler(renderer),
-        "/registration/user" bind Method.GET to GetUserRegistration(),
-        "/registration/user" bind Method.POST to PostUserRegistration(),
-        "/registration/project" bind Method.GET to GetProjectRegistration(),
-        "/registration/project" bind Method.POST to PostProjectRegistration(),
-        static(ResourceLoader.Classpath("/ru/uniyar/public/")),
-    )
+val app = routes(
+    "/" bind Method.GET to HomeHandler(),
+    "/projects" bind Method.GET to ProjectsHandler(),
+    "/projects/{id}" bind Method.GET to ProjectByIdHandler(),
+    "/registration/user" bind Method.GET to GetUserRegistration(),
+    "/registration/user" bind Method.POST to PostUserRegistration(),
+    "/registration/project" bind Method.GET to GetProjectRegistration(),
+    "/registration/project" bind Method.POST to PostProjectRegistration(),
+    static(ResourceLoader.Classpath("/ru/uniyar/public/")),
+)
 
 fun performMigrations() {
     val flyway = Flyway
@@ -90,18 +41,9 @@ fun performMigrations() {
     flyway.migrate()
 }
 
-val generatedUserModels =
-    listOf(
-        UserModel(0, "Alex", "Bond"),
-        UserModel(1, "Homa", "Round"),
-        UserModel(2, "Nastya", "Rukastai"),
-    )
-
 
 fun main() {
     performMigrations()
-    val renderer = PebbleTemplates().HotReload("./src/main/resources")
-    val app: HttpHandler = getRoutes(renderer)
 
     val server = app.asServer(Netty(port = 9000))
     server.start()
--- src/main/kotlin/ru/uniyar/web/handlers/HomeHandler.kt
+++ src/main/kotlin/ru/uniyar/web/handlers/HomeHandler.kt
@@ -5,10 +5,11 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.template.TemplateRenderer
+import ru.uniyar.Containers
 import ru.uniyar.web.models.HomePageViewModel
 
 class HomeHandler(
-    val renderer: TemplateRenderer,
+    val renderer: TemplateRenderer = Containers.renderer,
 ) : HttpHandler {
     override fun invoke(request: Request): Response {
         return Response(Status.OK).body(renderer(HomePageViewModel()))
